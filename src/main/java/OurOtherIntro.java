50a3ad0b136d9124645b3eda59a82df043bf6ac3
mrmirah <cmirah@dasd.org>
2020-08-09 16:38:46 -0400

    Init example
    folder structure, pom, gitignore

---
+++ src/main/java/OurOtherIntro.java
@@ -0,0 +1,16 @@
+//each program must have at least one class
+//to be able to run
+//a class is a blueprint of a concept
+//holds all the information about that thing
+//only one class per file may be public
+//the public class name should match the java file name
+//all code needs to be placed between the {} of the class
+
+    //each program also needs to have at least one main method
+    //execution starts in the main method and goes from top to
+    //bottom, might make detours along the way
+
+        //print messages to the console is one of the simplest
+        //starts to learning programming
+        //we print by using the System class
+
